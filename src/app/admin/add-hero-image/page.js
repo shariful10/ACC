5ce7f8bc904c75354325102ed1e65db24fc88889
Sk Shariful Islam <sksf211@gmail.com>
2023-11-17 15:59:48 +0600

    Input Field Implemented

--- src/app/admin/add-hero-image/page.js
+++ src/app/admin/add-hero-image/page.js
@@ -1,5 +1,5 @@
 import React from "react";
-import photo from "@/images/photo.svg"
+import photo from "@/images/photo.svg";
 import Image from "next/image";
 
 const AddHeroImage = () => {
@@ -14,19 +14,30 @@ const AddHeroImage = () => {
 						Add New
 					</button>
 				</div>
-            <p className="text-xl mt-5 md:mt-2">Share your product image. Image should be PNG and maximum size 1 Mb</p>
-            <div className="border-b border-b-[#050C2F] my-[30px]" />
-            <div>
-               <button className="bg-[#E4E4E4] rounded-[10px] px-[30px] py-3 flex items-center border border-[#878787]">
-                  <Image src={photo} className="mr-[10px]" alt="photo" />
-                  Add photo
-               </button>
-            </div>
+				<p className="text-xl mt-5 md:mt-2">
+					Share your product image. Image should be PNG and maximum size 1
+					Mb
+				</p>
+				<div className="border-b border-b-[#050C2F] my-[30px]" />
+				<div className="flex flex-col md:flex-row md:items-end">
+					<button className="bg-[#E4E4E4] rounded-[10px] px-[30px]  h-[50px] flex items-center border border-[#878787]">
+						<Image src={photo} className="mr-[10px]" alt="photo" />
+						Add photo
+					</button>
+               <div className="md:ml-5 mt-5 md:mt-0 flex flex-col">
+                  <label className="text-sm ml-2">Head Text (Maximum 8 words)</label>
+                  <input className="rounded-[10px] border border-[#050000] w-full md:w-[416px] px-[22px] focus:outline-none bg-transparent h-[50px] mt-3" type="text" placeholder="Write your Text" />
+               </div>
+				</div>
 			</div>
-         <div className="mt-8 mb-12 flex justify-end">
-            <button className="px-[18px] py-3 rounded-[10px] text-white bg-[#BD2626] hover:bg-red-800 duration-500">Save & Exit</button>
-            <button className="px-[18px] py-3 rounded-[10px] text-white bg-[#878787] hover:bg-gray-600 duration-500 ml-5">Publish</button>
-         </div>
+			<div className="mt-8 mb-12 flex justify-end">
+				<button className="px-[18px] py-3 rounded-[10px] text-white bg-[#BD2626] hover:bg-red-800 duration-500">
+					Save & Exit
+				</button>
+				<button className="px-[18px] py-3 rounded-[10px] text-white bg-[#878787] hover:bg-gray-600 duration-500 ml-5">
+					Publish
+				</button>
+			</div>
 		</div>
 	);
 };
